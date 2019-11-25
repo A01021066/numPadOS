212624b763ea1a35aef46cf275a10c9f9e3c43a6
A01021066 <liuouis@hotmail.com>
2019-11-24 16:52:41 -0800

    WE DID IT

---
+++ RawInput/PreMessageFilter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Runtime.InteropServices;
+
+namespace RawInput_dll
+{
+    public class PreMessageFilter : IMessageFilter
+    {
+
+        int x = 5;
+        // true  to filter the message and stop it from being dispatched 
+        // false to allow the message to continue to the next filter or control.
+
+        public bool PreFilterMessage(ref Message m)
+        {
+
+            return false;
+                //m.Msg == Win32.WM_KEYDOWN;
+        }
+    }
+}
